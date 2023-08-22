2875f22cd5d5c85bfcc73990b30a50a9c562cb29
Dave Allen <spondicious@me.com>
2023-08-22 23:10:02 +0200

    Locked up a couple o the pages so only seen to logged in users

--- cvdor/views.py
+++ cvdor/views.py
@@ -3,53 +3,50 @@ from .models import Parcela
 from .forms import ParcelaForm, ParcelaFormSm
 from django.http import HttpResponseRedirect
 
+# Pagination requirements
+from django.core.paginator import Paginator
 
+
+# SHOW HOME PAGE
 def home(request):
     return render(request, 'home.html', {})
 
 
+# SHOW ABOUT PAGE
 def about(request):
     return render(request, 'about.html', {})
 
 
+# LIST ALL PARCELAS
 def all_parcelas(request):
     parcela_list = Parcela.objects.all()
-    return render(request, 'list_parcelas.html', {'parcela_list': parcela_list})
+
+    # Pagination
+    p = Paginator(Parcela.objects.all(), 2)
+    page = request.GET.get('page')
+    parcelas = p.get_page(page)
+    nums = "a" * parcelas.paginator.num_pages
+
+    return render(request, 'list_parcelas.html', {'parcela_list': parcela_list, 'parcelas': parcelas, 'nums': nums})
 
 
-def show_parcela(request, parcela_id):
+# SHOW ONE PARCELA
+def show_parcela(request, parcela_id):    
     parcela = Parcela.objects.get(pk=parcela_id)
     return render(request, "show_parcela.html", {"parcela": parcela})
 
 
-
-# def update_parcela(request, parcela_id):
-#     parcela = Parcela.objects.get(pk=parcela_id)
-#     form = ParcelaForm(request.POST or None)
-#     if request.method == "POST":
-#         form = ParcelaForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect('/update_parcela?submitted=True')
-#     else:
-#         form = ParcelaForm
-#         if 'submitted' in request.GET:
-#             submitted = True
-#     return render(request, 'update_parcela.html', {"form": form, 'submitted': submitted})
-
-
-
+# UPDATE PARCELA
 def update_parcela(request, parcela_id):
     parcela = Parcela.objects.get(pk=parcela_id)
-    form = ParcelaForm(request.POST or None)
+    form = ParcelaForm(request.POST or None, instance=parcela)
     if form.is_valid():
         form.save()
         return redirect("all-parcelas")
     return render(request, "update_parcela.html", {"parcela": parcela, "form": form})
 
 
-
-# Add a new Parcela
+# ADD NEW pARCELA
 def add_parcela(request):
     submitted = False
     if request.method == "POST":
--- members/views.py
+++ members/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
-# from .forms import RegisterUserForm
+from .forms import RegisterUserForm
 
 # Login a user
 def login_user(request):
@@ -29,7 +29,7 @@ def logout_user(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -39,7 +39,7 @@ def register_user(request):
             messages.success(request, ("You have registered successfully.."))
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = RegisterUserForm()
 
 
     return render(request, 'authenticate/register_user.html', {'form': form,})
